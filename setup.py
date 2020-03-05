c68c8c52b4fa2bf2775ddc7fc7c504555df47f7f
monarin <monarin@gmail.com>
2020-03-05 14:23:27 -0800

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-monarin", 
+    version="0.0.1",
+    author="Monarin Uervirojnangkoorn",
+    author_email="monarin@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+
