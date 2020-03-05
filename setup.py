389dd5f9be4da6c438bed1e49a708473ad1b6c29
monarin <monarin@gmail.com>
2020-03-05 15:54:19 -0800

    more stuffs

--- setup.py
+++ setup.py
@@ -10,4 +10,13 @@ setuptools.setup(
     author_email="monarin@gmail.com",
     description="A small example package",
     long_description=long_description,
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/monarin/packaging_tutorial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
